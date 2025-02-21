be3258e9defd1c036fcf89dc8351e3133dacf992
jerry <chenyuan.wang@mail.utoronto.ca>
2025-02-20 19:16:15 -0600

    Train adversaries using rule based good agent

--- mat_algorithms/train_adversaries.py
+++ mat_algorithms/train_adversaries.py
@@ -16,7 +16,7 @@ import os
 
 # Initialize the environment with only 1 adversary and 1 good agent
 def custom_env():
-    env = simple_tag_v3.parallel_env(num_good=1, num_adversaries=1, num_obstacles=1, max_cycles=100, render_mode=None)
+    env = simple_tag_v3.parallel_env(num_good=n_good_agents, num_adversaries=n_adversaries, num_obstacles=1, max_cycles=100, render_mode="human")
     env.reset()
 
     # Keep only one adversary and one good agent
@@ -118,7 +118,7 @@ save_dir = f"models/"
 os.makedirs(save_dir, exist_ok=True)
 
 # Directory to load model
-model_dir = f"models/transformer_Jan16_20-43-20.pt"
+model_dir = f"models/transformer_Feb20_18-15-33.pt"
 
 # COPIED functions
 
@@ -211,8 +211,11 @@ def train_adversary(env, episodes=1000, gamma=0.99, save_interval=100):
             log_prob = action_log_probs[0, 0]
             value = values_return[0, 0]
 
-            # Manually set the good agent's action
-            good_action = fixed_action_policy(observations, good_agent)
+            good_agent_actions = []
+            for i in range(n_good_agents):
+                # Manually set the good agent's action
+                # good_agent_actions.append(fixed_action_policy(observations, good_agents[i]))
+                good_agent_actions.append(rule_based_action_policy(observations, good_agents[i]))
 
             # Step the environment with both agents
             actions_dict = {adversary: action, good_agent: good_action}
